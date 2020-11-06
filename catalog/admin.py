9926006647ec861bdfd66df69babf6f198d838e7
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-11-06 21:08:14 +0530

    Add BookInline in Author & Update BookInstance admin view

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,9 +9,14 @@ from .models import Author, Book, Genre, BookInstance, Language
 # admin.site.register(BookInstance)
 # admin.site.register(Language)
 
+class BookInline(admin.TabularInline):
+    model = Book
+
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 admin.site.register(Author, AuthorAdmin)
 
@@ -28,6 +33,8 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance) 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+
+    list_display = ('id', 'book', 'due_back')
 
     fieldsets = (
         (None, {
