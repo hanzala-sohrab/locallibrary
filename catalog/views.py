437097557563aa6ae374a971c2ad491d5c7b705e
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-11-07 01:19:21 +0530

    Add Index page view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from catalog.models import Book, Author, Genre, Language, BookInstance
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    numberOfBooks = Book.objects.all().count()
+    numberOfInstances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    numberOfInstancesAvailable = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    numberOfAuthors = Author.objects.count()
+
+    context = {
+        'noOfBooks': numberOfBooks,
+        'noOfInstances': numberOfInstances,
+        'noOfInstAvailable': numberOfInstancesAvailable,
+        'noOfAuthors': numberOfAuthors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
