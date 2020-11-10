1c829637a46264283ce920ba0cb960cba8e746d7
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-11-10 23:20:43 +0530

    Add Class Based List & Detail View for Authors

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # Books
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+    # Authors
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
