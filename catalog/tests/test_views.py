344faad64b6f6cb4c623a6b10e646c8cba6e58c8
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-11-18 14:49:23 +0530

    Add TODO

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -303,3 +303,5 @@ class RenewBookInstancesViewTest(TestCase):
                                     {'renewal_date': invalid_date_in_future})
         self.assertEqual(response.status_code, 200)
         self.assertFormError(response, 'form', 'renewal_date', 'Invalid date - renewal more than 4 weeks ahead!')
+
+# TODO: Implement AuthorCreateView
