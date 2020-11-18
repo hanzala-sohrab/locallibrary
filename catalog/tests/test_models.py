7e9a975d74651367f997831d7b200d852b3c3a25
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-11-18 11:26:25 +0530

    Add model test for max_length of last_name

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -34,6 +34,11 @@ class AuthorModelTest(TestCase):
         max_length = author._meta.get_field('first_name').max_length
         self.assertEqual(max_length, 100)
 
+    def test_last_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('last_name').max_length
+        self.assertEqual(max_length, 100)
+
     def test_object_name_is_last_name_comma_first_name(self):
         author = Author.objects.get(id=1)
         expected_object_name = f'{author.last_name}, {author.first_name}'
